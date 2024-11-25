72f67b37864680335be453a24e42c765b35efbb2
MasahAlmahdi <almahdi911i@gmail.com>
2024-11-25 02:19:21 +0200

    [feat] repair

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import UserList from './components/UserList/UserList';
+import { Layout } from 'antd';
+import AddUserForm from './components/AddUserForm/AddUserForm';
+import { useEffect, useState } from 'react';
+
+const {Content} = Layout;
 
 function App() {
+   const [localUsers, setLocalUsers] = useState(() => {
+    const savedUsers = localStorage.getItem("localUsers");
+    return savedUsers ? JSON.parse(savedUsers) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("localUsers", JSON.stringify(localUsers));
+  }, [localUsers]);
+
+  const addUser = (newUser) => {
+    setLocalUsers((prev) => [...prev, newUser]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sidebar/>
+      <Layout>
+        <Content style={{ padding: '24px' }}>
+        <Routes>
+          <Route path='/' element={<UserList localUsers={localUsers}/>}/>
+          <Route path='/add-user' element={<AddUserForm onUserAdded={addUser} />}/>
+        </Routes>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
